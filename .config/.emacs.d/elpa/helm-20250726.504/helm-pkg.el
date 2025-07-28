c0b153bc5092ccae4458103567dcb9d8fd29b47d
Harry Newman <h.newman2005@outlook.com>
2025-07-28 21:48:41 +0100

    Added emacs

---
+++ .config/.emacs.d/elpa/helm-20250726.504/helm-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm" "20250726.504"
+  "Helm is an Emacs incremental and narrowing framework."
+  '((helm-core "4.0.4")
+    (wfnames   "1.2"))
+  :url "https://emacs-helm.github.io/helm/"
+  :commit "2c1bbb104ea68379a4a46a7410471ad701513fde"
+  :revdesc "2c1bbb104ea6"
+  :keywords '("helm" "convenience" "files" "buffers" "grep" "completion" "lisp" "matching" "tools" "help")
+  :authors '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers '(("Thierry Volpiatto" . "thievol@posteo.net")))
---
+++ .config/.emacs.d/transient/history.el
@@ -0,0 +1,4 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-gitignore nil)
+ (magit-push nil))
